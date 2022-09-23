75dcb0f1a5a2bf7dd7a27a4d48329b72e947998e
Parunyu Kia <parunyu.kia@gmail.com>
2022-09-24 05:24:14 +0700

    Improve test1
    Improve function call api and html.
    Add note.

--- Test1/Test.js
+++ Test1/Test.js
@@ -1,18 +1,20 @@
 // https://ftx.com/api/markets/BTC/USDT <-- original API
-const ftx = "https://cors-anywhere.herokuapp.com/https://ftx.com/api/markets/BTC/USDT";
+// Use https://cors-anywhere.herokuapp.com to Fix CORS block
+const ftx ="https://cors-anywhere.herokuapp.com/https://ftx.com/api/markets/BTC/USDT";
 const binance = "https://api1.binance.com/api/v3/avgPrice?symbol=BTCUSDT";
-show()
-function getapi(url){
-    const respone = fetch(url).then(async(data)=>{
-        return await data.json();
-    }).catch((error) => {
-        console.log(error)
-        return
-    });
-    return respone
+show();
+
+async function getapi(url) {
+  const respone = await fetch(url);
+  if (respone.ok) {
+    return respone.json();
+  } else {
+    throw new Error("API has error.");
+  }
 }
 
-async function show(){
+async function show() {
+  try {
     // Get price
     let ftxShow = await getapi(ftx);
     let binanceShow = await getapi(binance);
@@ -23,20 +25,25 @@ async function show(){
     document.getElementById("FTX").innerHTML = `${ftxRes} USDT`;
     document.getElementById("Binance").innerHTML = `${binanceRes} USDT`;
     // Diff
-    ftxRes > binanceRes ? diffhandle(ftxRes,binanceRes):diffhandle(binanceRes,ftxRes);
+    ftxRes > binanceRes
+      ? diffhandle(ftxRes, binanceRes)
+      : diffhandle(binanceRes, ftxRes);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-function diffhandle(first,second){
-    let diff = Math.abs(first - second);
-    let perdiff = 100 * Math.abs(diff / ((first + second)/2));
-    document.getElementById("diff").innerHTML = `${diff} USDT`
-    document.getElementById("perdiff").innerHTML = `( ${perdiff} % )`
+function diffhandle(first, second) {
+  let diff = Math.abs(first - second);
+  let perdiff = 100 * Math.abs(diff / ((first + second) / 2));
+  document.getElementById("diff").innerHTML = `${diff} USDT`;
+  document.getElementById("perdiff").innerHTML = `( ${perdiff} % )`;
 }
 
-function update(){
-    document.getElementById("FTX").innerHTML = "Downloading result..."
-    document.getElementById("Binance").innerHTML = "Downloading result..."
-    document.getElementById("diff").innerHTML = "Downloading result..."
-    document.getElementById("perdiff").innerHTML = ""
-    show()
+function update() {
+  document.getElementById("FTX").innerHTML = "Downloading result...";
+  document.getElementById("Binance").innerHTML = "Downloading result...";
+  document.getElementById("diff").innerHTML = "Downloading result...";
+  document.getElementById("perdiff").innerHTML = "";
+  show();
 }
