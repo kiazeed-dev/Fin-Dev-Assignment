2b7001293ed9c1a3f7495ec8075693ab057952c8
Parunyu Kia <parunyu.kia@gmail.com>
2022-09-24 08:23:05 +0700

    Clean Up.

--- CORSdrill/index.js
+++ CORSdrill/index.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
